f261323400cdcde3c67d13d3fc3f095f39840889
oleksii.kav <oleksii.kav@gmail.com>
2020-05-08 14:17:09 +0300

    update: js task 1

--- GL-js-hw-01/task-03.js
+++ GL-js-hw-01/task-03.js
@@ -1,10 +1,12 @@
 const ADMIN_PASSWORD = 'qwerty';
 let message;
+// eslint-disable-next-line prefer-const
 message = prompt('Enter you password: ');
 if (message === null) {
   console.log('Отменено пользователем!');
 } else if (message === ADMIN_PASSWORD) {
   console.log('Добро пожаловать!');
+  console.log('sadf');
 } else {
   console.log('Доступ запрещен, неверный пароль!');
 }
